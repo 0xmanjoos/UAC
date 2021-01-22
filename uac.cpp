6455a03be02b46b1ac84d064f060cfbdffb684de
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2021-01-21 21:36:22 -0800

    Update uac.cpp

--- uac.cpp
+++ uac.cpp
@@ -3,9 +3,9 @@
 bool admin() {
     bool fRet = FALSE;
     HANDLE hToken = NULL;
-    if( OpenProcessToken( GetCurrentProcess( ),TOKEN_QUERY,&hToken ) ) {
+    if( OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY,&hToken)) {
         TOKEN_ELEVATION Elevation;
-        DWORD cbSize = sizeof( TOKEN_ELEVATION );
+        DWORD cbSize = sizeof(TOKEN_ELEVATION);
         if(GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
             fRet = Elevation.TokenIsElevated;
         }
